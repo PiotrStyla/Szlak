30ed9ffb5a5c614545770bf7b8df4379e62c0936
Admin <p.styla@gmail.com>
2019-06-27 10:29:37 +0200

    dni Zwiastowania na razie buttony tylko+

--- app/src/main/java/pl/diecezja/zr/Zwiastowanie.kt
+++ app/src/main/java/pl/diecezja/zr/Zwiastowanie.kt
@@ -4,16 +4,24 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.SpinnerAdapter
+import android.widget.*
 
 class Zwiastowanie : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_zwiastowanie)
+
+        val button_zwiastowanie_do_wstep  = findViewById (R.id.button_Zwiastowanie_Wstep) as Button
+        button_zwiastowanie_do_wstep.setOnClickListener {
+            val intent_zwiastowanie_wstep = Intent(this, ZwiastowanieWstep::class.java)
+            startActivity(intent_zwiastowanie_wstep)
+        }
+        val button_zwiastowanie_do_dzien1  = findViewById (R.id.button27) as Button
+        button_zwiastowanie_do_dzien1.setOnClickListener {
+            val intent_zwiastowanie_dzien1 = Intent(this, ZwiastowanieDzien1::class.java)
+            startActivity(intent_zwiastowanie_dzien1)
+        }
 
 
         }

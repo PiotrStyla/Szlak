526b17e57af4db9b3684431bde64d084f69de4b9
Admin <p.styla@gmail.com>
2019-10-05 17:08:23 +0200

    dodane odnalezienie wstęp

--- app/src/main/java/pl/diecezja/zr/Odnalezienie.kt
+++ app/src/main/java/pl/diecezja/zr/Odnalezienie.kt
@@ -1,12 +1,20 @@
 package pl.diecezja.zr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Odnalezienie : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.odnalezienie_activity_odnalezienie)
+
+        val buttonOdnalezienieDoWstep = findViewById<Button>(R.id.button_Odnalezienie_Wstep)
+        buttonOdnalezienieDoWstep.setOnClickListener {
+            val intentOdnalezienieWstep = Intent(this, OdnalezienieWstep::class.java)
+            startActivity(intentOdnalezienieWstep)
+        }
     }
 }

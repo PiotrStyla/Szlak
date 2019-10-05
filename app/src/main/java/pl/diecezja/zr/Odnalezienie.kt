87ca88f67bf506929742c305c89f2b09e4da271a
Admin <p.styla@gmail.com>
2019-10-05 17:50:18 +0200

    dodane odnalezienie dzień 1

--- app/src/main/java/pl/diecezja/zr/Odnalezienie.kt
+++ app/src/main/java/pl/diecezja/zr/Odnalezienie.kt
@@ -16,5 +16,155 @@ class Odnalezienie : AppCompatActivity() {
             val intentOdnalezienieWstep = Intent(this, OdnalezienieWstep::class.java)
             startActivity(intentOdnalezienieWstep)
         }
+        val buttonOdnalezienieDoDzien1 = findViewById<Button>(R.id.button145)
+        buttonOdnalezienieDoDzien1.setOnClickListener {
+            val intentOdnalezienieDzien1 = Intent(this, OdnalezienieDzien1::class.java)
+            startActivity(intentOdnalezienieDzien1)
+        }
+        val buttonOfiarowanieDoDzien2 = findViewById<Button>(R.id.button115)
+        buttonOfiarowanieDoDzien2.setOnClickListener {
+            val intentOfiarowanieDzien2 = Intent(this, OfiarowanieDzien2::class.java)
+            startActivity(intentOfiarowanieDzien2)
+        }
+        val buttonOfiarowanieDoDzien3 = findViewById<Button>(R.id.button116)
+        buttonOfiarowanieDoDzien3.setOnClickListener {
+            val intentOfiarowanieDzien3 = Intent(this, OfiarowanieDzien3::class.java)
+            startActivity(intentOfiarowanieDzien3)
+        }
+        val buttonOfiarowanieDoDzien4 = findViewById<Button>(R.id.button117)
+        buttonOfiarowanieDoDzien4.setOnClickListener {
+            val intentOfiarowanieDzien4 = Intent(this, OfiarowanieDzien4::class.java)
+            startActivity(intentOfiarowanieDzien4)
+        }
+        val buttonOfiarowanieDoDzien5 = findViewById<Button>(R.id.button118)
+        buttonOfiarowanieDoDzien5.setOnClickListener {
+            val intentOfiarowanieDzien5 = Intent(this, OfiarowanieDzien5::class.java)
+            startActivity(intentOfiarowanieDzien5)
+        }
+        val buttonOfiarowanieDoDzien6 = findViewById<Button>(R.id.button119)
+        buttonOfiarowanieDoDzien6.setOnClickListener {
+            val intentOfiarowanieDzien6 = Intent(this, OfiarowanieDzien6::class.java)
+            startActivity(intentOfiarowanieDzien6)
+        }
+        val buttonOfiarowanieDoDzien7 = findViewById<Button>(R.id.button120)
+        buttonOfiarowanieDoDzien7.setOnClickListener {
+            val intentOfiarowanieDzien7 = Intent(this, OfiarowanieDzien7::class.java)
+            startActivity(intentOfiarowanieDzien7)
+        }
+        val buttonOfiarowanieDoDzien8 = findViewById<Button>(R.id.button121)
+        buttonOfiarowanieDoDzien8.setOnClickListener {
+            val intentOfiarowanieDzien8 = Intent(this, OfiarowanieDzien8::class.java)
+            startActivity(intentOfiarowanieDzien8)
+        }
+        val buttonOfiarowanieDoDzien9 = findViewById<Button>(R.id.button122)
+        buttonOfiarowanieDoDzien9.setOnClickListener {
+            val intentOfiarowanieDzien9 = Intent(this, OfiarowanieDzien9::class.java)
+            startActivity(intentOfiarowanieDzien9)
+        }
+        val buttonOfiarowanieDoDzien10 = findViewById<Button>(R.id.button123)
+        buttonOfiarowanieDoDzien10.setOnClickListener {
+            val intentOfiarowanieDzien10 = Intent(this, OfiarowanieDzien10::class.java)
+            startActivity(intentOfiarowanieDzien10)
+        }
+        val buttonOfiarowanieDoDzien11 = findViewById<Button>(R.id.button124)
+        buttonOfiarowanieDoDzien11.setOnClickListener {
+            val intentOfiarowanieDzien11 = Intent(this, OfiarowanieDzien11::class.java)
+            startActivity(intentOfiarowanieDzien11)
+        }
+        val buttonOfiarowanieDoDzien12 = findViewById<Button>(R.id.button125)
+        buttonOfiarowanieDoDzien12.setOnClickListener {
+            val intentOfiarowanieDzien12 = Intent(this, OfiarowanieDzien12::class.java)
+            startActivity(intentOfiarowanieDzien12)
+        }
+        val buttonOfiarowanieDoDzien13 = findViewById<Button>(R.id.button126)
+        buttonOfiarowanieDoDzien13.setOnClickListener {
+            val intentOfiarowanieDzien13 = Intent(this, OfiarowanieDzien13::class.java)
+            startActivity(intentOfiarowanieDzien13)
+        }
+        val buttonOfiarowanieDoDzien14 = findViewById<Button>(R.id.button127)
+        buttonOfiarowanieDoDzien14.setOnClickListener {
+            val intentOfiarowanieDzien14 = Intent(this, OfiarowanieDzien14::class.java)
+            startActivity(intentOfiarowanieDzien14)
+        }
+        val buttonOfiarowanieDoDzien15 = findViewById<Button>(R.id.button128)
+        buttonOfiarowanieDoDzien15.setOnClickListener {
+            val intentOfiarowanieDzien15 = Intent(this, OfiarowanieDzien15::class.java)
+            startActivity(intentOfiarowanieDzien15)
+        }
+        val buttonOfiarowanieDoDzien16 = findViewById<Button>(R.id.button129)
+        buttonOfiarowanieDoDzien16.setOnClickListener {
+            val intentOfiarowanieDzien16 = Intent(this, OfiarowanieDzien16::class.java)
+            startActivity(intentOfiarowanieDzien16)
+        }
+        val buttonOfiarowanieDoDzien17 = findViewById<Button>(R.id.button130)
+        buttonOfiarowanieDoDzien17.setOnClickListener {
+            val intentOfiarowanieDzien17 = Intent(this, OfiarowanieDzien17::class.java)
+            startActivity(intentOfiarowanieDzien17)
+        }
+        val buttonOfiarowanieDoDzien18 = findViewById<Button>(R.id.button131)
+        buttonOfiarowanieDoDzien18.setOnClickListener {
+            val intentOfiarowanieDzien18 = Intent(this, OfiarowanieDzien18::class.java)
+            startActivity(intentOfiarowanieDzien18)
+        }
+        val buttonOfiarowanieDoDzien19 = findViewById<Button>(R.id.button132)
+        buttonOfiarowanieDoDzien19.setOnClickListener {
+            val intentOfiarowanieDzien19 = Intent(this, OfiarowanieDzien19::class.java)
+            startActivity(intentOfiarowanieDzien19)
+        }
+        val buttonOfiarowanieDoDzien20 = findViewById<Button>(R.id.button133)
+        buttonOfiarowanieDoDzien20.setOnClickListener {
+            val intentOfiarowanieDzien20 = Intent(this, OfiarowanieDzien20::class.java)
+            startActivity(intentOfiarowanieDzien20)
+        }
+        val buttonOfiarowanieDoDzien21 = findViewById<Button>(R.id.button134)
+        buttonOfiarowanieDoDzien21.setOnClickListener {
+            val intentOfiarowanieDzien21 = Intent(this, OfiarowanieDzien21::class.java)
+            startActivity(intentOfiarowanieDzien21)
+        }
+        val buttonOfiarowanieDoDzien22 = findViewById<Button>(R.id.button135)
+        buttonOfiarowanieDoDzien22.setOnClickListener {
+            val intentOfiarowanieDzien22 = Intent(this, OfiarowanieDzien22::class.java)
+            startActivity(intentOfiarowanieDzien22)
+        }
+        val buttonOfiarowanieDoDzien23 = findViewById<Button>(R.id.button136)
+        buttonOfiarowanieDoDzien23.setOnClickListener {
+            val intentOfiarowanieDzien23 = Intent(this, OfiarowanieDzien23::class.java)
+            startActivity(intentOfiarowanieDzien23)
+        }
+        val buttonOfiarowanieDoDzien24 = findViewById<Button>(R.id.button137)
+        buttonOfiarowanieDoDzien24.setOnClickListener {
+            val intentOfiarowanieDzien24 = Intent(this, OfiarowanieDzien24::class.java)
+            startActivity(intentOfiarowanieDzien24)
+        }
+        val buttonOfiarowanieDoDzien25 = findViewById<Button>(R.id.button138)
+        buttonOfiarowanieDoDzien25.setOnClickListener {
+            val intentOfiarowanieDzien25 = Intent(this, OfiarowanieDzien25::class.java)
+            startActivity(intentOfiarowanieDzien25)
+        }
+        val buttonOfiarowanieDoDzien26 = findViewById<Button>(R.id.button139)
+        buttonOfiarowanieDoDzien26.setOnClickListener {
+            val intentOfiarowanieDzien26 = Intent(this, OfiarowanieDzien26::class.java)
+            startActivity(intentOfiarowanieDzien26)
+        }
+        val buttonOfiarowanieDoDzien27 = findViewById<Button>(R.id.button140)
+        buttonOfiarowanieDoDzien27.setOnClickListener {
+            val intentOfiarowanieDzien27 = Intent(this, OfiarowanieDzien27::class.java)
+            startActivity(intentOfiarowanieDzien27)
+        }
+        val buttonOfiarowanieDoDzien28 = findViewById<Button>(R.id.button141)
+        buttonOfiarowanieDoDzien28.setOnClickListener {
+            val intentOfiarowanieDzien28 = Intent(this, OfiarowanieDzien28::class.java)
+            startActivity(intentOfiarowanieDzien28)
+        }
+        val buttonOfiarowanieDoDzien29 = findViewById<Button>(R.id.button142)
+        buttonOfiarowanieDoDzien29.setOnClickListener {
+            val intentOfiarowanieDzien29 = Intent(this, NarodzenieDzien29::class.java)
+            startActivity(intentOfiarowanieDzien29)
+        }
+        val buttonOfiarowanieDoDzien30 = findViewById<Button>(R.id.button143)
+        buttonOfiarowanieDoDzien30.setOnClickListener {
+            val intentOfiarowanieDzien30 = Intent(this, NarodzenieDzien30::class.java)
+            startActivity(intentOfiarowanieDzien30)
+        }
     }
 }

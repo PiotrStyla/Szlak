fdf0cf5d7b468089a7a6132f8f1b9bb27616cf5a
Admin <p.styla@gmail.com>
2019-11-06 19:37:15 +0100

    menu info

--- app/src/main/java/pl/diecezja/zr/MainActivity.kt
+++ app/src/main/java/pl/diecezja/zr/MainActivity.kt
@@ -1,8 +1,12 @@
 package pl.diecezja.zr
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,4 +28,32 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+    //menu start
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            if(item.itemId == R.id.action_about){
+                showInfo()
+            }
+        }
+        return true
+    }
+
+    @SuppressLint("StringFormatInvalid")
+    private fun showInfo(){
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+
+    }
+    //menu end
 }

c34587edb99e365e36e8c61b5283d078647ce8cf
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2019-11-30 07:27:07 +0100

    1.0.2 + starting Swiatla +Jordan wstep

--- app/src/main/java/pl/diecezja/zr/Jordan.kt
+++ app/src/main/java/pl/diecezja/zr/Jordan.kt
@@ -1,12 +1,20 @@
 package pl.diecezja.zr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Jordan : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.jordan_activity_jordan)
+
+        val buttonJordanDoWstep = findViewById<Button>(R.id.button_Jordan_Wstep)
+        buttonJordanDoWstep.setOnClickListener {
+            val intentJordanWstep = Intent(this, JordanWstep::class.java)
+            startActivity(intentJordanWstep)
+        }
     }
 }
---
+++ app/src/main/java/pl/diecezja/zr/JordanWstep.kt
@@ -0,0 +1,12 @@
+package pl.diecezja.zr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class JordanWstep : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jordan_wstep_activity_jordan_wstep)
+    }
+}

7a96870edd6be911f549d7d09b4e9358a6a03081
Piotr Styła <31997939+PiotrStyla@users.noreply.github.com>
2020-03-08 18:33:41 +0100

    Galilea wstęp

--- app/src/main/java/pl/diecezja/zr/Galilea.kt
+++ app/src/main/java/pl/diecezja/zr/Galilea.kt
@@ -1,12 +1,170 @@
 package pl.diecezja.zr
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Galilea : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.galilea_activity_galilea)
+
+        val buttonGalileaDoWstep = findViewById<Button>(R.id.button_Galilea_Wstep)
+        buttonGalileaDoWstep.setOnClickListener {
+            val intentGalileaWstep = Intent(this, GalileaWstep::class.java)
+            startActivity(intentGalileaWstep)
+        }
+        val buttonKanaDzien1 = findViewById<Button>(R.id.button114kana)
+        buttonKanaDzien1.setOnClickListener {
+            val intentKanaDzien1 = Intent(this, KanaDzien1::class.java)
+            startActivity(intentKanaDzien1)
+        }
+        val buttonKanaDzien2 = findViewById<Button>(R.id.button115kana)
+        buttonKanaDzien2.setOnClickListener {
+            val intentKanaDzien2 = Intent(this, KanaDzien2::class.java)
+            startActivity(intentKanaDzien2)
+        }
+        val buttonKanaDzien3 = findViewById<Button>(R.id.button116kana)
+        buttonKanaDzien3.setOnClickListener {
+            val intentKanaDzien3 = Intent(this, KanaDzien3::class.java)
+            startActivity(intentKanaDzien3)
+        }
+        val buttonKanaDzien4 = findViewById<Button>(R.id.button117kana)
+        buttonKanaDzien4.setOnClickListener {
+            val intentKanaDzien4 = Intent(this, KanaDzien4::class.java)
+            startActivity(intentKanaDzien4)
+        }
+        val buttonKanaDzien5 = findViewById<Button>(R.id.button118kana)
+        buttonKanaDzien5.setOnClickListener {
+            val intentKanaDzien5 = Intent(this, KanaDzien5::class.java)
+            startActivity(intentKanaDzien5)
+        }
+        val buttonKanaDzien6 = findViewById<Button>(R.id.button119kana)
+        buttonKanaDzien6.setOnClickListener {
+            val intentKanaDzien6 = Intent(this, KanaDzien6::class.java)
+            startActivity(intentKanaDzien6)
+        }
+        val buttonKanaDzien7 = findViewById<Button>(R.id.button120kana)
+        buttonKanaDzien7.setOnClickListener {
+            val intentKanaDzien7 = Intent(this, KanaDzien7::class.java)
+            startActivity(intentKanaDzien7)
+        }
+        val buttonKanaDzien8 = findViewById<Button>(R.id.button121kana)
+        buttonKanaDzien8.setOnClickListener {
+            val intentKanaDzien8 = Intent(this, KanaDzien8::class.java)
+            startActivity(intentKanaDzien8)
+        }
+        val buttonKanaDzien9 = findViewById<Button>(R.id.button122kana)
+        buttonKanaDzien9.setOnClickListener {
+            val intentKanaDzien9 = Intent(this, KanaDzien9::class.java)
+            startActivity(intentKanaDzien9)
+        }
+        val buttonKanaDzien10 = findViewById<Button>(R.id.button123kana)
+        buttonKanaDzien10.setOnClickListener {
+            val intentKanaDzien10 = Intent(this, KanaDzien10::class.java)
+            startActivity(intentKanaDzien10)
+        }
+        val buttonKanaDzien11 = findViewById<Button>(R.id.button124kana)
+        buttonKanaDzien11.setOnClickListener {
+            val intentKanaDzien11 = Intent(this, KanaDzien11::class.java)
+            startActivity(intentKanaDzien11)
+        }
+        val buttonKanaDzien12 = findViewById<Button>(R.id.button125kana)
+        buttonKanaDzien12.setOnClickListener {
+            val intentKanaDzien12 = Intent(this, KanaDzien12::class.java)
+            startActivity(intentKanaDzien12)
+        }
+        val buttonKanaDzien13 = findViewById<Button>(R.id.button126kana)
+        buttonKanaDzien13.setOnClickListener {
+            val intentKanaDzien13 = Intent(this, KanaDzien13::class.java)
+            startActivity(intentKanaDzien13)
+        }
+        val buttonKanaDzien14 = findViewById<Button>(R.id.button127kana)
+        buttonKanaDzien14.setOnClickListener {
+            val intentKanaDzien14 = Intent(this, KanaDzien14::class.java)
+            startActivity(intentKanaDzien14)
+        }
+        val buttonKanaDzien15 = findViewById<Button>(R.id.button128kana)
+        buttonKanaDzien15.setOnClickListener {
+            val intentKanaDzien15 = Intent(this, KanaDzien15::class.java)
+            startActivity(intentKanaDzien15)
+        }
+        val buttonKanaDzien16 = findViewById<Button>(R.id.button129kana)
+        buttonKanaDzien16.setOnClickListener {
+            val intentKanaDzien16 = Intent(this, KanaDzien16::class.java)
+            startActivity(intentKanaDzien16)
+        }
+        val buttonKanaDzien17 = findViewById<Button>(R.id.button130kana)
+        buttonKanaDzien17.setOnClickListener {
+            val intentKanaDzien17 = Intent(this, KanaDzien17::class.java)
+            startActivity(intentKanaDzien17)
+        }
+        val buttonKanaDzien18 = findViewById<Button>(R.id.button131kana)
+        buttonKanaDzien18.setOnClickListener {
+            val intentKanaDzien18 = Intent(this, KanaDzien18::class.java)
+            startActivity(intentKanaDzien18)
+        }
+        val buttonKanaDzien19 = findViewById<Button>(R.id.button132kana)
+        buttonKanaDzien19.setOnClickListener {
+            val intentKanaDzien19 = Intent(this, KanaDzien19::class.java)
+            startActivity(intentKanaDzien19)
+        }
+        val buttonKanaDzien20 = findViewById<Button>(R.id.button133kana)
+        buttonKanaDzien20.setOnClickListener {
+            val intentKanaDzien20 = Intent(this, KanaDzien20::class.java)
+            startActivity(intentKanaDzien20)
+        }
+        val buttonKanaDzien21 = findViewById<Button>(R.id.button134kana)
+        buttonKanaDzien21.setOnClickListener {
+            val intentKanaDzien21 = Intent(this, KanaDzien21::class.java)
+            startActivity(intentKanaDzien21)
+        }
+        val buttonKanaDzien22 = findViewById<Button>(R.id.button135kana)
+        buttonKanaDzien22.setOnClickListener {
+            val intentKanaDzien22 = Intent(this, KanaDzien22::class.java)
+            startActivity(intentKanaDzien22)
+        }
+        val buttonKanaDzien23 = findViewById<Button>(R.id.button136kana)
+        buttonKanaDzien23.setOnClickListener {
+            val intentKanaDzien23 = Intent(this, KanaDzien23::class.java)
+            startActivity(intentKanaDzien23)
+        }
+        val buttonKanaDzien24 = findViewById<Button>(R.id.button137kana)
+        buttonKanaDzien24.setOnClickListener {
+            val intentKanaDzien24 = Intent(this, KanaDzien24::class.java)
+            startActivity(intentKanaDzien24)
+        }
+        val buttonKanaDzien25 = findViewById<Button>(R.id.button138kana)
+        buttonKanaDzien25.setOnClickListener {
+            val intentKanaDzien25 = Intent(this, KanaDzien25::class.java)
+            startActivity(intentKanaDzien25)
+        }
+        val buttonKanaDzien26 = findViewById<Button>(R.id.button139kana)
+        buttonKanaDzien26.setOnClickListener {
+            val intentKanaDzien26 = Intent(this, KanaDzien26::class.java)
+            startActivity(intentKanaDzien26)
+        }
+        val buttonKanaDzien27 = findViewById<Button>(R.id.button140kana)
+        buttonKanaDzien27.setOnClickListener {
+            val intentKanaDzien27 = Intent(this, KanaDzien27::class.java)
+            startActivity(intentKanaDzien27)
+        }
+        val buttonKanaDzien28 = findViewById<Button>(R.id.button141kana)
+        buttonKanaDzien28.setOnClickListener {
+            val intentKanaDzien28 = Intent(this, KanaDzien28::class.java)
+            startActivity(intentKanaDzien28)
+        }
+        val buttonKanaDzien29 = findViewById<Button>(R.id.button142kana)
+        buttonKanaDzien29.setOnClickListener {
+            val intentKanaDzien29 = Intent(this, KanaDzien29::class.java)
+            startActivity(intentKanaDzien29)
+        }
+        val buttonKanaDzien30 = findViewById<Button>(R.id.button143kana)
+        buttonKanaDzien30.setOnClickListener {
+            val intentKanaDzien30 = Intent(this, KanaDzien30::class.java)
+            startActivity(intentKanaDzien30)
+        }
     }
 }
---
+++ app/src/main/java/pl/diecezja/zr/GalileaWstep.kt
@@ -0,0 +1,12 @@
+package pl.diecezja.zr
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GalileaWstep : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.galilea_wstep_activity_galilea_wstep)
+    }
+}
